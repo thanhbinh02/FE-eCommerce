46ef6d831832f1e063f52969449562ebf2cbf0a4
Đinh Thị Thanh Bình <thanhbinh57kt@mac-thanh-binh.local>
2024-10-06 21:14:31 +0700

    feat:(error-message) remove showErrorMessage

--- src/utils/error-message.tsx
+++ src/utils/error-message.tsx
@@ -1,19 +1,3 @@
-import { message } from "antd";
-
-import { DEFAULT_MESSAGE_ERROR } from "@/data/constant";
-import { ResponseError } from "@/ts";
-
 export type ErrorMessages<T extends string> = {
   [key in T]: string;
 };
-
-export function showErrorMessage<T extends string>(
-  error: ResponseError<T>,
-  obj: ErrorMessages<T>,
-) {
-  if (error.response?.data.message) {
-    void message.error(obj[error.response?.data.message]);
-  } else {
-    void message.error(DEFAULT_MESSAGE_ERROR);
-  }
-}
