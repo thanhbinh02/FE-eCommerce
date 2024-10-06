16bb848df8fa20e390d1e904c9012aebf3c72017
Đinh Thị Thanh Bình <thanhbinh57kt@mac-thanh-binh.local>
2024-10-06 21:11:20 +0700

    chore:(auth) remove showErrorMessage

--- src/features/auth/hooks/use-auth-query.ts
+++ src/features/auth/hooks/use-auth-query.ts
@@ -1,23 +1,9 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { message } from "antd";
 
-import {
-  LOGIN_ERROR_MESSAGE,
-  OTP_VALID_DURATION,
-  REFRESH_OTP_ERROR_MESSAGE,
-  RESET_PASSWORD_ERROR_MESSAGE,
-  VERIFY_OTP_ERROR_MESSAGE,
-} from "../constant";
+import { OTP_VALID_DURATION } from "../constant";
 import authApi from "../services/auth-api";
-import {
-  TLoginErrors,
-  TRefreshOTPErrors,
-  TResetPasswordErrors,
-  TVerifyOTPErrors,
-} from "../services/types";
 import { useAppLocalStorage } from "@/features/local-storage";
-import { ResponseError } from "@/ts";
-import { showErrorMessage } from "@/utils";
 
 export const useLoginMutation = () => {
   const { setResetToken } = useAppLocalStorage();
@@ -34,28 +20,17 @@ export const useLoginMutation = () => {
 
       void message.success("Xác thực tài khoản thành công. Vui lòng nhập OTP.");
     },
-    onError: (error: ResponseError<TLoginErrors>) => {
-      void showErrorMessage(error, LOGIN_ERROR_MESSAGE);
-    },
   });
 };
 
 export const useVerifyOTPMutation = () => {
-  const { removeAllAuthData } = useAppLocalStorage();
-
   return useMutation({
     mutationFn: authApi.verityOTP,
-    onError: (error: ResponseError<TVerifyOTPErrors>) => {
-      void showErrorMessage(error, VERIFY_OTP_ERROR_MESSAGE);
-      if (error.response?.data.message === "system_message_token_not_found") {
-        removeAllAuthData();
-      }
-    },
   });
 };
 
 export const useRefreshOTPMutation = (isForgotPassword: boolean) => {
-  const { setResetToken, removeAllAuthData } = useAppLocalStorage();
+  const { setResetToken } = useAppLocalStorage();
 
   return useMutation({
     mutationFn: authApi.refreshOTP,
@@ -68,12 +43,6 @@ export const useRefreshOTPMutation = (isForgotPassword: boolean) => {
       });
 
       void message.success("Mã OTP mới đã được gửi! Vui lòng kiểm tra Email.");
-    },
-    onError: (error: ResponseError<TRefreshOTPErrors>) => {
-      void showErrorMessage(error, REFRESH_OTP_ERROR_MESSAGE);
-      if (error.response?.data.message === "system_message_token_not_found") {
-        removeAllAuthData();
-      }
     },
   });
 };
@@ -95,18 +64,12 @@ export const useResetPasswordMutation = () => {
         "Mã OTP tới Email thành công! Vui lòng kiểm tra Email.",
       );
     },
-    onError: (error: ResponseError<TResetPasswordErrors>) => {
-      void showErrorMessage(error, RESET_PASSWORD_ERROR_MESSAGE);
-    },
   });
 };
 
 export const useCreatePasswordMutation = () => {
   return useMutation({
     mutationFn: authApi.createPassword,
-    onError: (error: ResponseError<TResetPasswordErrors>) => {
-      void showErrorMessage(error, RESET_PASSWORD_ERROR_MESSAGE);
-    },
   });
 };
 
