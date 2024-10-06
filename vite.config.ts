81956e8b58ddbbfcef526b1d3f116ce6cd7a301b
Đinh Thị Thanh Bình <thanhbinh57kt@mac-thanh-binh.local>
2024-10-06 21:15:42 +0700

    Add env, vite.config.ts, gitgnore

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,10 @@ import svgrPlugin from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 3001,
   },
   preview: {
-    port: 3000,
+    port: 3001,
   },
   resolve: {
     alias: {
